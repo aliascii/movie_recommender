c858e82d25f43f73442c761e4b7993641717410d
Your Name <aliasci@live.de>
2023-05-03 14:38:21 +0200

    movie recommender up and running

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from recommender import random_recommender, MOVIES
+from recommender import recommend_nmf
+
 
 app = Flask(__name__)
 
@@ -7,10 +8,15 @@ app = Flask(__name__)
 def homepage(): 
     return render_template('home.html')
 
-@app.route('/results') #Decorator to take function's output and giving it back to user in a browser (rendering)
-def recommender():
-    top_two = random_recommender()
-    return f'{top_two}'
+# with open('./Factorizer_NMF_2.pkl', 'rb') as file_in:
+#     fitted_model = pickle.load(file_in)
+
+# @app.route('/results') #Decorator to take function's output and giving it back to user in a browser (rendering)
+# def recommender():
+#     user_query = request.args.to_dict()
+#     user_query = {key:int(value) for key, value in user_query.items()}
+#     top_two = recommendations()
+#     return render_template('/results.html', movies = top_two)
 
 # @app.route('/results/')
 # def recommender():
@@ -24,15 +30,16 @@ def recommender():
 #     top_two = co_sim()
 #     return render_template('results.html', movies = top_two)
 
-# @app.route('/NMF_results')
-# def recommender():
-#     user_query = request.args.to_dict()
-#     user_query = {key:int(value) for key, value in user_query.items()}
-#     top_two = NMF()
-#     return render_template('results.html', movies = top_two)
+@app.route('/results')
+def myrecommendation():
+    user_query = request.args.to_dict()
+    user_query = {key:int(value) for key, value in user_query.items()}
+    #print(user_query)
+    recommendations = recommend_nmf(query=user_query)
+    return render_template('/results.html', movies = recommendations)
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(debug=True, port=5000)
 
 
 #'q=jungle+book&oq=jungle+book&aqs=chrome.0.0i271j46i340i433i512l3j0i512j46i512j0i512j46i512j0i512l2.2001j0j7&sourceid=chrome&ie=UTF-8'
--- recommender.py
+++ recommender.py
@@ -1,21 +1,74 @@
-'''Implements functions for making predictions.'''
-import random
+# 1. Load the necessary libraries
+import pandas as pd
+import numpy as np
+import pickle
+import warnings
+warnings.filterwarnings("ignore")
 
-# ratings = pd.read_csv()
 
-MOVIES = [
-    'Avatar',
-    'The Great Beauty',
-    'Star Wars',
-    'Interstellar'
-]
+# 2. Load the dataset containing user ratings and movie titles
+user_movie = pd.read_csv('./user_movie.csv', index_col=0)
 
-def random_recommender():
-    random.shuffle(MOVIES)
-    top_two = MOVIES[0:2]
-    return top_two
+# 3. Preprocess the data and perform feature engineering if necessary
+# You may need to normalize the ratings or impute missing values
 
-# def cos_sim():
-#     pass
+# 4. Split the data into training and testing sets
+# Depending on the size of your dataset, you may need to use cross-validation or other techniques to evaluate your model
 
-# def NMF():
+# 5. Train an unsupervised learning model
+with open('./Factorizer_NMF_2.pkl', 'rb') as file_in:
+    fitted_model = pickle.load(file_in)
+
+# 6. Use the trained model to generate movie recommendations for a given user
+def recommend_nmf(query, model=fitted_model, k=10):
+    """
+    Filters and recommends the top k movies for any given input query based on a trained NMF model. 
+    Returns a list of k movie ids.
+    """
+    # define matrices
+    USERS = list(user_movie.index)
+    MOVIES = list(user_movie.columns)
+    Q = fitted_model.components_
+    
+    # define movie rating average
+    movie_mean = user_movie.mean()
+
+    # 1. candidate generation
+    user_input = pd.DataFrame(query, index=['new_user'], columns=MOVIES)
+    user_input_imputed = user_input.fillna(value=movie_mean)
+
+    # 2. construct new_user-item dataframe given the query
+    P_user = model.transform(user_input_imputed)
+    P_user = pd.DataFrame(P_user, index=['new_user'])
+
+    # 3. scoring
+    R_user_hat = np.dot(P_user, Q)
+    R_user_hat = pd.DataFrame(R_user_hat, columns=MOVIES, index=['new_user'])
+
+    # 4. ranking
+    R_user_hat_transposed = R_user_hat.T.sort_values(by='new_user', ascending=False)
+
+    # filter out movies already seen by the user
+    user_initial_ratings_list = list(query.keys())
+
+    # return the top-k highest rated movie ids or titles
+    recommendables = list(R_user_hat_transposed.index)
+    recommendations = [movie for movie in recommendables if movie not in user_initial_ratings_list][:k]
+    return recommendations
+
+
+
+# return the top-k recommended movies to the user
+if __name__ =="__main__":
+    # example query
+    query = {
+        "Salem's Lot (2004)": 0,
+        "Til There Was You (1997)": 5,
+        "Round Midnight (1986)": 5
+    }
+
+    # generate recommendations for the given query
+    recommendations = recommend_nmf(query, fitted_model)
+    k = 10
+    top_k_recommendations = recommendations[:k]
+    print(top_k_recommendations)
